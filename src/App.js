bc96d5eda22af00d8fe8425749c14f5ec81ab8b1
Mathias Hedström <hedstrom.mathias@gmail.com>
2018-04-16 13:04:24 +0200

    v3.0.3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import oops from './oops.png'
 let Raven = require('raven-js')
 
 Raven.config('https://9e381a0287464529af7a8a88edc27c9b@sentry.io/210938', {
-  release: '3.0.2'
+  release: '3.0.3'
 }).install()
 
 /* global gtag */
